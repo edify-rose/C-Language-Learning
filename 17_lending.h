41d73ef76a83450da237a05c7232576fe27cf4fe
edify-rose <abundance.enthusiasm@gmail.com>
2026-08-31 21:04:53 +0900

    Add files via upload

---
+++ 17_lending.h
@@ -0,0 +1,8 @@
+#ifndef LENDING_H	//‘Ý‚µo‚µ‹@”\
+#define LENDING_H
+#if D_STEP >= 3
+
+
+
+#endif	// D_STEP >= 3
+#endif	// LENDING_H
---
+++ 17_search.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_H	//ŒŸõ‹@”\
+#define SEARCH_H
+#if D_STEP >= 4
+
+
+
+#endif	// D_STEP >= 4
+#endif	// SEARCH_H
